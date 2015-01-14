9cfc6fdf8578b07abe1ee8b1097f21539bf047f3
Peter Kassenaar <info@kassenaar.com>
2015-01-14 15:38:54 +0100

    Voorbeeld custom event via $broadcast toegevoegd (ng-13-xxx-02)

---
+++ codevoorbeelden/angularJS/js/13_ng_controller02.js
@@ -0,0 +1,36 @@
+﻿(function (app) {
+
+	// 1. Maak de controller
+	var bookController = function ($scope, bookFactory) {
+		// 2. Call naar methode in de factory, gebruik promise-notatie 
+		bookFactory.getBooks().success(function (data) {
+			$scope.books = data;
+		});
+
+		// Luister naar event dat wordt gegooid vanuit de interceptor
+		$scope.$on('my-custom-event', function(event, data){
+			$scope.eventData = data;
+		});
+
+		// Luister naar 'ready' event dat wordt gegooid vanuit de interceptor
+		$scope.$on('my-custom-event-ready', function(event, data){
+			$scope.eventReadyData = data;
+		});
+	};
+
+	// 2. Maak de detailcontroller (kan ook in aparte file, nu even gecombineerd)
+	var detailController = function ($scope, bookFactory, $routeParams) {
+		console.log($routeParams);
+		bookFactory.getBookDetail($routeParams.ean)
+			.success(function (data) {
+				console.log(data);
+				$scope.book = data;
+				$scope.imgSrc = 'http://content.yindo.nl/images/covers/' + data.ean + '.jpg';
+			});
+	};
+
+	// 3. Controllers toevoegen aan app
+	app.controller('bookController', ['$scope', 'bookFactory', bookController]);
+	app.controller('detailController', ['$scope', 'bookFactory', '$routeParams', detailController]);
+
+})(angular.module('myApp')); // bestaande module doorgeven als parameter
---
+++ codevoorbeelden/angularJS/js/13_ng_factory02.js
@@ -0,0 +1,59 @@
+﻿(function (app) {
+	//
+	// Eerste factory/service op deze app, de Interceptor (om XHR-requests te onderscheppen)
+	//
+	app.factory('myInterceptor', function ($q, $rootScope) {
+		// 1. Maak een 'factory'-object
+		var interceptor = {};
+
+		// 2. Functies request en response te onderscheppen
+		interceptor.request = function (config) {
+			// broadcast custom event, stuur config.url mee als data
+			$rootScope.$broadcast('my-custom-event', config.url)
+			// Return the config or wrap it in a promise if blank.
+			return config|| $q.when(config);
+		};
+
+		// 2. Functies om success te onderscheppen
+		interceptor.response = function (config) {
+			// broadcast custom event, stuur config.url mee als data
+			$rootScope.$broadcast('my-custom-event-ready', config.data)
+			// Return the config or wrap it in a promise if blank.
+			return config|| $q.when(config);
+		};
+
+		// 4. Return factory object
+		return interceptor;
+	});
+
+	//
+	// De tweede factory op deze app, de oorspronkelijke bookFactory (om boekgegevens op te halen)
+	//
+	app.factory('bookFactory', function ($http) {
+		// 1. Maak een 'factory'-object
+		var factory = {};
+
+		// 2. Definieer URL waar gegevens worden opgehaald (hier: Yindo API)
+		var url = 'http://api.yindo.com/api/book/new/10',
+			urlDetails = 'http://api.yindo.com/api/book/details/';
+
+		// 3. Definieer functies als API/interface voor de buitenwereld
+		factory.getBooks = function () {
+		    return $http({
+		        method: 'jsonp',
+		        url: url + '?callback=JSON_CALLBACK'
+		    });
+		};
+
+		factory.getBookDetail = function (ean) {
+		    return $http({
+		    	method: 'jsonp',
+
+		        url: urlDetails + ean + '?callback=JSON_CALLBACK'
+		    });
+		};
+
+		// 4. Altijd tot slot: retourneer het factory-object
+		return factory;
+	});
+})(angular.module('myApp'));
