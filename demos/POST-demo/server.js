5e449bbfdd823fd0473dc0410ee321ec88333fd9
Peter Kassenaar <info@kassenaar.com>
2015-11-20 10:12:59 +0100

    verbeterde POST Demo en $http-factory

--- demos/POST-demo/server.js
+++ demos/POST-demo/server.js
@@ -1,21 +1,21 @@
-var express    = require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
-var jwt        = require('jwt-simple');
-var _          = require('lodash');
-var app        = express();
+var jwt = require('jwt-simple');
+var _ = require('lodash');
+var app = express();
 //*******************
 // Parse forms & JSON in body
 //*******************
 app.use(bodyParser.urlencoded({
-	extended : true
+    extended: true
 }));
 app.use(bodyParser.json());
 
 //Stel middleware in voor serveren van statische bestanden (HTML, CSS, images)
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-	res.send('Gebruik: stuur een POST-request met user-gegevens(bv "username" en "email") naar http://localhost:3000/user')
+app.get('/api', function (req, res) {
+    res.send('Gebruik: stuur een POST-request met user-gegevens(bv "username" en "email") naar http://localhost:3000/user')
 });
 
 //****************
@@ -23,18 +23,25 @@ app.get('/', function (req, res) {
 //****************
 var user = {};
 app.post('/user', function (req, res) {
-	// verwerk binnenkomende request. We gaan er van uit
-	// dat de parameter 'username' en 'email' aanwezig zijn.
-	// TODO: error checking!
-	console.log(req.body);
-	user.username = req.body.username;
-	user.email    = req.body.email;
-	// Echo het user-object naar de client
-	res.json(user);
+    // verwerk binnenkomende request. We gaan er van uit
+    // dat de parameter 'username' en 'email' aanwezig zijn.
+    // TODO: error checking!
+    console.log(req.body);
+    user.username = req.body.username;
+    user.email = req.body.email;
+    // In een echte app: HIER valideren en naar de
+    // database voor live data. Voor nu:
+    // Echo het user-object naar de client
+    // Eventueel;
+    //user.token = "1233434536456.778789900";
+    res.json(user);
 
 });
-
 
 app.listen(3000, function () {
-	console.log('Server gestart op poort 3000...');
+    console.log('Server gestart op poort 3000...');
 });
+
+//**************
+// TODO: Echte authenticatie via JWT, zie bijvoorbeeld https://github.com/auth0/angularjs-jwt-authentication-tutorial
+//*************
--- oefeningen/js/11_ng_factory01.js
+++ oefeningen/js/11_ng_factory01.js
@@ -1,66 +1,67 @@
 ﻿(function () {
-	angular.module('myApp')
-		.factory('bookFactory', function ($http) {
-		// 1. Maak een 'factory'-object
-		var factory = {};
+    angular.module('myApp')
+        .factory('bookFactory', function ($http) {
+            // 1. Maak een 'factory'-object
+            var factory = {};
 
-		// 2. Definieer URL waar gegevens worden opgehaald (hier: Yindo API)
-		var urlYindo = 'http://api.yindo.com/api/book/new/10';
+            // 2. Definieer URL waar gegevens worden opgehaald (hier: Yindo API)
+            var urlYindo = 'http://api.yindo.com/api/book/new/10';
 
-		// 3. Definieer functies als API/interface voor de buitenwereld
-		// 3a. Voorbeeld 1: Boekgegevens ophalen vanaf Yindo
-		factory.getBooks = function () {
-			return $http({
-				method: 'jsonp',
-				url: urlYindo + '?callback=JSON_CALLBACK',
-                cache:true
-			});
-		};
+            // 3. Definieer functies als API/interface voor de buitenwereld
+            // 3a. Voorbeeld 1: Boekgegevens ophalen vanaf Yindo
+            factory.getBooks = function () {
+                return $http({
+                    method: 'jsonp',
+                    url: urlYindo + '?callback=JSON_CALLBACK',
+                    cache: true
+                });
+            };
 
-		// 3b. Voorbeeld 2: Dummy persoonsgegevens ophalen vanaf FillText.com
-		//var urlPeople = 'http://filltext.com/?rows=10&fname={firstName}&lname={lastName}&email={email}&id={index}';
-		//factory.getPeople = function () {
-		//	return $http({
-		//		method:'jsonp',
-		//		url: urlPeople,
-		//		params: { 'callback' : 'JSON_CALLBACK' }
-		//	});
-		//}
+            // 3b. Voorbeeld 2: Dummy persoonsgegevens ophalen vanaf FillText.com
+            //var urlPeople = 'http://filltext.com/?rows=10&fname={firstName}&lname={lastName}&email={email}&id={index}';
+            //factory.getPeople = function () {
+            //	return $http({
+            //		method:'jsonp',
+            //		url: urlPeople,
+            //		params: { 'callback' : 'JSON_CALLBACK' }
+            //	});
+            //}
 
-		// 3c. Voorbeeld 3: Boeken ophalen, maar ALLEEN id, titel en auteur teruggeven.
-		//    i.e. een stukje BI in de service, in plaats van in de controller (==keuze van de maker).
+            // 3c. Voorbeeld 3: Boeken ophalen, maar ALLEEN id, titel en auteur teruggeven.
+            //    i.e. een stukje BI in de service, in plaats van in de controller (==keuze van de maker).
 
-		var partialBooks = [];
-		var loadPartialBooks = (function () {
-			return $http({
-				method: 'jsonp',
-				url: urlYindo + '?callback=JSON_CALLBACK'
-			});
-		})().success(function (data) {
-			for (var i = 0; i < data.length; i++) {
-				// mapping van je properties
+            //var partialBooks = [];
+            //var loadPartialBooks = (function () {
+            //	return $http({
+            //		method: 'jsonp',
+            //		url: urlYindo + '?callback=JSON_CALLBACK'
+            //	});
+            //})().success(function (data) {
+            //	for (var i = 0; i < data.length; i++) {
+            //		// mapping van je properties
+            //
+            //		partialBooks.push({
+            //			name: data[i].firstAuthorName,
+            //			id: data[i].firstAuthorID,
+            //			title: data[i].title,
+            //			language : data[i].languageID== 1 ? 'Nederlands' : 'Buitenlands'
+            //		});
+            //	}
+            //});
+            //
+            //factory.getPartialBooks = function () {
+            //	return partialBooks;
+            //};
 
-				partialBooks.push({
-					name: data[i].firstAuthorName,
-					id: data[i].firstAuthorID,
-					title: data[i].title,
-					language : data[i].languageID== 1 ? 'Nederlands' : 'Buitenlands'
-				});
-			}
-		});
+            // Voorbeeld 4 : Weergegevens ophalen via openweatherMap. Sinds 1-11-2015: AppID (=api key) verplicht. zelf aanvragen op http://home.openweathermap.org/
+            factory.getWeather = function (city) {
+                return $http({
+                    method: 'get',
+                    url: 'http://api.openweathermap.org/data/2.5/weather?units=metric&appid=8566d604cd9402b65394b034e52aa2af&q=' + city
+                });
+            };
 
-		factory.getPartialBooks = function () {
-			return partialBooks;
-		};
-
-		factory.getWeather = function (city) {
-			return $http({
-				method: 'get',
-				url: 'http://api.openweathermap.org/data/2.5/weather?units=metric&appid=8566d604cd9402b65394b034e52aa2af&q=' + city
-			});
-		};
-
-		// 4. Altijd tot slot: retourneer het factory-object
-		return factory;
-	});
+            // 5. Altijd tot slot: retourneer het factory-object
+            return factory;
+        });
 })();
