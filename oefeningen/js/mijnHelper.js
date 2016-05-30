a9393ef3f5196b23a94c21252ebb86caec890c4b
Peter Kassenaar <info@kassenaar.com>
2016-05-30 14:09:18 +0200

    Update mijnHelper.js

--- oefeningen/js/mijnHelper.js
+++ oefeningen/js/mijnHelper.js
@@ -2,11 +2,12 @@
 // met helperFuncties. Deze module wordt straks in stap 2
 // geinjecteerd in de 'hoofd'module "mijnApp". De helpermodule
 // heeft 1 controller met een dummy-functie clickMe()
-angular.module('mijnHelper', [])
-	.controller('alertController', function ($scope) {
-		$scope.clickMe = function () {
-			alert('Hello World vanuit de Helpermodule');
-		}
-	});
-
+function(){
+	angular.module('mijnHelper', [])
+		.controller('alertController', function ($scope) {
+			$scope.clickMe = function () {
+				alert('Hello World vanuit de Helpermodule');
+			}
+		});
+})();
 
