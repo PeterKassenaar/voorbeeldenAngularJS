6501c14a75b770fc467830643e43b89a5c34c74b
Peter Kassenaar <info@kassenaar.com>
2016-05-31 10:06:09 +0200

    Update voorbeeld $routeProvider

--- oefeningen/js/09_app.js
+++ oefeningen/js/09_app.js
@@ -4,29 +4,32 @@
 
 	// 2. Routes configureren
 	angular.module('myApp')
-		.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider
-				.when('/', {
-					templateUrl: 'views/09_view01.html', // Default view
-					controller: 'personController'
-				})
-				.when('/namen', {
-					templateUrl: 'views/09_view01.html',
-					controller: 'personController'
-				})
-				.when('/detail/:id/:name?', {
-					templateUrl: 'views/09_viewDetail.html',
-					controller: 'detailController',
-					controllerAs: 'd'
-				})
-				.when('/steden', {
-					templateUrl: 'views/09_view02.html',
-					controller: 'citiesController',
-					controllerAs: 'c'
-				})
-				.when('/404', {
-					templateUrl: 'views/404.html'
-				})
-				 .otherwise({ redirectTo: '/404' });
-		}]);
+		.config(configRoutes)
+		
+	configRoutes.$inject = ['$routeProvider'];
+	function configRoutes ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/09_view01.html', // Default view
+				controller: 'personController'
+			})
+			.when('/namen', {
+				templateUrl: 'views/09_view01.html',
+				controller: 'personController'
+			})
+			.when('/detail/:id/:name?', {
+				templateUrl: 'views/09_viewDetail.html',
+				controller: 'detailController',
+				controllerAs: 'd'
+			})
+			.when('/steden', {
+				templateUrl: 'views/09_view02.html',
+				controller: 'citiesController',
+				controllerAs: 'c'
+			})
+			.when('/404', {
+				templateUrl: 'views/404.html'
+			})
+			 .otherwise({ redirectTo: '/404' });
+	};
 })();
