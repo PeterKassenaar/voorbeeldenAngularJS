b72c708f5eec0b51ecd3e19a9677c1c068cbfeee
Peter Kassenaar <info@kassenaar.com>
2016-06-01 11:22:05 +0200

    Update 13_app02.js

--- oefeningen/js/13_app02.js
+++ oefeningen/js/13_app02.js
@@ -1,7 +1,12 @@
 ﻿(function () {
 	// Definieer de module/app in dit bestand en voeg (later) eventuele dependencies toe
 	var app = angular.module('myApp', ['ngRoute']);
-	app.config(function ($routeProvider, $httpProvider) {
+	app.config(myConfig);
+
+	myConfig.$inject = ['$routeProvider', '$httpProvider'];
+	function myConfig($routeProvider, $httpProvider) {
+		$httpProvider.interceptors.push('myInterceptor');
+		
 		$routeProvider
 			.when('/', {
 				templateUrl : 'views/13_view02.html' // Default (en enige) view in dit eenvoudige project
@@ -10,11 +15,7 @@
 				templateUrl : 'views/13_view_detail.html'
 			})
 			.otherwise({redirectTo : '/'});
-
-		// push de interceptor-factory op de $httpProvider-module (ze zijn gedefinieerd in 13_ng_factory01.js)
-		$httpProvider.interceptors.push('myInterceptor');
-
-	});
+	};
 
 	app.constant('GLOBALS', {
 		customEvent      : 'my-custom-event',
