0ff38754ad7028777343c984186c25cf6e485c9b
Peter Kassenaar <info@kassenaar.com>
2016-09-20 14:16:59 +0200

    Bijgewerkt voorbeeld $http

--- oefeningen/js/11_ng_controller01.js
+++ oefeningen/js/11_ng_controller01.js
@@ -9,16 +9,14 @@
 		// Dit kan NIET (want: async):
 		//$scope.books = bookFactory.getBooks();
 
-
-		// 2. Call naar methode in de factory, gebruik promise-notatie 
+		// 2. Call naar methode in de factory, gebruik promise-notatie
 		bookFactory.getBooks()
 			.success(function (boeken) {
 				console.log(boeken);
 				$scope.books = boeken;
 			}).error(function (err) {
-				alert('Er is iets fout gegaan: ', err);
-			});
-
+			alert('Er is iets fout gegaan: ', err);
+		});
 
 		// Mapping van *delen* van boeken naar een custom book object?
 		// Dan zul je een Service (of Factory) moeten schrijven die zelf
@@ -27,24 +25,23 @@
 		// $scope.partialBooks = bookFactory.getPartialBooks();
 
 		// 3. Dummmy persoonsgegevens ophalen
-		//bookFactory.getPeople()
-		//	.success(function (data, status, headers, config) {
-		//		$scope.people = data;
-		//	})
-		//	.error(function (data, status, headers, config) {
-		//		alert('Error bij Ajax-call: ', status);
-		//	})
-
+		bookFactory.getPeople()
+			.success(function (data, status, headers, config) {
+				$scope.people = data;
+			})
+			.error(function (data, status, headers, config) {
+				alert('Error bij Ajax-call: ', status);
+			})
 
 		// 4. Weer ophalen in een bepaalde stad
 
 		$scope.showWeather = false;
-		$scope.getWeather = function () {
+		$scope.getWeather  = function () {
 			var city = $scope.city;
 			bookFactory.getWeather(city).success(function (weatherData) {
 				console.log(weatherData);
 				$scope.plaatsnaam = weatherData.name;
-				$scope.graden = weatherData.main.temp;
+				$scope.graden     = weatherData.main.temp;
 				//$scope.showWeather = true;
 			});
 		};
@@ -52,11 +49,10 @@
 		$scope.resetWeather = function () {
 			if ($scope.city.length === 0) {
 				$scope.plaatsnaam = null;
-				$scope.graden = null;
+				$scope.graden     = null;
 			}
 			console.log($scope.city);
 		}
-
 
 	}
 })(); // bestaande module doorgeven als parameter
