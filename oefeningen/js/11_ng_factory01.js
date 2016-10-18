1b0aca248ac47e35eb1e61c0d2660106d8644841
Peter Kassenaar <info@kassenaar.com>
2016-10-18 17:11:31 +0200

    Update 11_ng_factory01.js

--- oefeningen/js/11_ng_factory01.js
+++ oefeningen/js/11_ng_factory01.js
@@ -19,7 +19,7 @@
 			});
 		};
 
-		// 3b. Voorbeeld 2: Dummy persoonsgegevens ophalen vanaf FillText.com
+		// 3b. Voorbeeld 2: Dummy persoonsgegevens ophalen vanaf FillText.com, plus andere notatie parameters
 		var urlPeople     = 'http://filltext.com/?rows=10&fname={firstName}&lname={lastName}&email={email}&id={index}';
 		factory.getPeople = function () {
 			return $http({
@@ -28,32 +28,6 @@
 				params : {'callback' : 'JSON_CALLBACK'}
 			});
 		}
-
-		// 3c. Voorbeeld 3: Boeken ophalen, maar ALLEEN id, titel en auteur teruggeven.
-		//    i.e. een stukje BI in de service, in plaats van in de controller (==keuze van de maker).
-
-		//var partialBooks = [];
-		//var loadPartialBooks = (function () {
-		//	return $http({
-		//		method: 'jsonp',
-		//		url: urlYindo + '?callback=JSON_CALLBACK'
-		//	});
-		//})().success(function (data) {
-		//	for (var i = 0; i < data.length; i++) {
-		//		// mapping van je properties
-		//
-		//		partialBooks.push({
-		//			name: data[i].firstAuthorName,
-		//			id: data[i].firstAuthorID,
-		//			title: data[i].title,
-		//			language : data[i].languageID== 1 ? 'Nederlands' : 'Buitenlands'
-		//		});
-		//	}
-		//});
-		//
-		//factory.getPartialBooks = function () {
-		//	return partialBooks;
-		//};
 
 		// Voorbeeld 4 : Weergegevens ophalen via openweatherMap. Sinds 1-11-2015: AppID (=api key) verplicht. zelf aanvragen op http://home.openweathermap.org/
 		factory.getWeather = function (city) {
