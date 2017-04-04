15bc935d2c8f71f9e4f6e373a8b40a767c276f6c
Peter Kassenaar <info@kassenaar.com>
2017-04-04 18:14:35 +0200

    Uitbreiding 11_http met Spotify API

--- oefeningen/js/11_app.js
+++ oefeningen/js/11_app.js
@@ -1,17 +1,15 @@
 ﻿(function () {
-    // Definieer de module/app in dit bestand en voeg (later) eventuele dependencies toe
-    angular.module('myApp', ['ngRoute'])
-        .config(configRoutes);
+	// Definieer de module/app in dit bestand en voeg (later) eventuele dependencies toe
+	var app = angular.module('myApp', ['ngRoute', 'ngSanitize']);
+	app.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {				
+				templateUrl: 'views/11_view01.html' // Default (en enige) view in dit eenvoudige project
+			})
+			.when('/namen', {				
+				templateUrl: 'views/11_view01.html'
+			})		
+		.otherwise({ redirectTo: '/' });
 
-    configRoutes.$inject = ['$routeProvider'];
-    function configRoutes($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/11_view01.html' // Default (en enige) view in dit eenvoudige project
-            })
-            .when('/namen', {
-                templateUrl: 'views/11_view01.html'
-            })
-            .otherwise({redirectTo: '/'});
-    }
+	});
 })();
--- oefeningen/js/11_ng_controller01.js
+++ oefeningen/js/11_ng_controller01.js
@@ -1,58 +1,101 @@
-﻿(function (app) {
+﻿(function () {
 
-	angular.module('myApp')
-		.controller('bookController', bookController);
+	var app            = angular.module('myApp');
 	// 1. Maak de controller
-	bookController.$inject = ['$scope', 'bookFactory'];
-	function bookController($scope, bookFactory) {
+	var bookController = function ($scope, bookFactory, $location, $http, $sce) {
 
 		// Dit kan NIET (want: async):
-		//$scope.books = bookFactory.getBooks();
+		// $scope.books = bookFactory.getBooks();
 
-		// 2. Call naar methode in de factory, gebruik promise-notatie
-		bookFactory.getBooks()
-			.success(function (boeken) {
-				console.log(boeken);
-				$scope.books = boeken;
-			}).error(function (err) {
-			alert('Er is iets fout gegaan: ', err);
-		});
+		$scope.getBooks = function () {
+			// 2. Call naar methode in de factory, gebruik promise-notatie
+			bookFactory.getBooks()
+				.success(function (bookData) {
+					$scope.books = bookData;
+				})
+				.error(function (err) {
+					alert('FOUT! ' + err)
+				});
 
-		// Mapping van *delen* van boeken naar een custom book object?
+		};
 		// Dan zul je een Service (of Factory) moeten schrijven die zelf
 		// een stukje business logic heeft en pas retourneert als de
 		// items uit de http-call terug zijn gekomen.
 		// $scope.partialBooks = bookFactory.getPartialBooks();
 
 		// 3. Dummmy persoonsgegevens ophalen
-		bookFactory.getPeople()
-			.success(function (data, status, headers, config) {
-				$scope.people = data;
+		//bookFactory.getPeople()
+		//  .success(function (data, status, headers, config) {
+		//    $scope.people = data;
+		//  })
+		//  .error(function (data, status, headers, config) {
+		//    alert('Error bij Ajax-call: ', status);
+		//  })
+
+		var spotifyAPI = 'https://api.spotify.com/v1/search?type=track&query=';
+
+		$scope.getArtist = function (artist) {
+			$http({
+				method : 'GET',
+				url    : spotifyAPI + artist
 			})
-			.error(function (data, status, headers, config) {
-				alert('Error bij Ajax-call: ', status);
-			})
-
-		// 4. Weer ophalen in een bepaalde stad
-
-		$scope.showWeather = false;
-		$scope.getWeather  = function () {
-			var city = $scope.city;
-			bookFactory.getWeather(city).success(function (weatherData) {
-				console.log(weatherData);
-				$scope.plaatsnaam = weatherData.name;
-				$scope.graden     = weatherData.main.temp;
-				//$scope.showWeather = true;
-			});
-		};
-
-		$scope.resetWeather = function () {
-			if ($scope.city.length === 0) {
-				$scope.plaatsnaam = null;
-				$scope.graden     = null;
-			}
-			console.log($scope.city);
+				.then(function (artistData) {
+					console.log(artistData.data.tracks);
+					// Transformatie van het return-resultaat
+					var artistTracks = [];
+					artistData.data.tracks.items.forEach(function (track) {
+						var newTrack = {
+							cover   : track.album.images[0].url,
+							name    : track.name,
+							preview : $sce.trustAsResourceUrl(track.preview_url)
+						};
+						artistTracks.push(newTrack)
+					});
+					// Na de transformatie het resultaat toekennen aan de $scope
+					$scope.tracks = artistTracks;
+				})
 		}
 
-	}
-})(); // bestaande module doorgeven als parameter
+	};
+
+	app.controller('bookController', ['$scope', 'bookFactory', '$location', '$http', '$sce', bookController]);
+
+	////////////////////////////
+	// Weathercontroller maken
+	////////////////////////////
+	angular.module('myApp')
+		.controller('weatherController', weatherController);
+
+	weatherController.$inject = ['bookFactory'];
+
+	function weatherController(bookFactory) {
+
+		var vm         = this;
+		//vm.city = window.localStorage.getItem('city')
+		vm.plaatsnaam  = '';
+		vm.graden      = '';
+		vm.showWeather = '';
+		vm.getWeather  = function () {
+			var city = vm.city;
+			var db   = window.localStorage;
+
+			// Set item in localStorage;
+			db.setItem('city', city);
+
+			bookFactory.getWeather(city)// 1. praat tegen service
+				.then(function (weatherData) { //2. ontvang result
+					console.log(weatherData);
+					vm.plaatsnaam  = weatherData.data.name;// 3. bind aan HTML
+					vm.graden      = weatherData.data.main.temp;
+					vm.showWeather = true;
+				});
+
+			vm.resetWeather = function () {
+				vm.plaatsnaam  = '';// 3. bi
+				vm.graden      = '';
+				vm.showWeather = false;
+			}
+		};
+	}// end WeatherController
+
+})();
