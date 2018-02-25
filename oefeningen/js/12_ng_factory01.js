4a75fb41d2701608d702bedb446ca5acb65a43a9
Peter Kassenaar <info@kassenaar.com>
2018-02-25 21:01:30 +0100

    Update 12_ng_factory01.js

--- oefeningen/js/12_ng_factory01.js
+++ oefeningen/js/12_ng_factory01.js
@@ -1,38 +1,35 @@
-﻿(function (app) {
-	app.factory('bookFactory', function ($http) {
+(function () {
+	angular.module('myApp')
+		.factory('movieFactory', movieFactory);
+
+	movieFactory.$inject = ['$http'];
+
+	function movieFactory($http) {
 		// 1. Maak een 'factory'-object
 		var factory = {};
 
-		// 2. Definieer URL's waar gegevens worden opgehaald (hier: Yindo API)
-		var url = 'http://api.yindo.com/api/book/new/10',
-			urlDetails = 'http://api.yindo.com/api/book/details/',
-			urlGetPage = 'http://api.yindo.com/api/page/load/00000000-0000-0000-0000-000000000000/'; // standaard : even als anonieme gebruiker boekpagina ophalen
+		// 2. Definieer URL's waar gegevens worden opgehaald (hier: OMDB API)
+		var url = 'http://omdbapi.com/?apikey=f1f56c8e&s=', 		// Let op de 's=' parameter,
+			urlDetails = 'http://omdbapi.com/?apikey=f1f56c8e&i=';	// let op de 't=' parameter, voor ophalen details
 
-		// 3a. Definieer functies als API/interface voor de buitenwereld. Als eerste de globale functie getBooks()
-		factory.getBooks = function () {
+		// 3a. Definieer functies als API/interface voor de buitenwereld.
+		// Als eerste de globale functie getMovies(), die Hardcoded alle 'avatar' films ophaalt
+		factory.getMovies = function () {
 			return $http({
-				method: 'jsonp',
-				url: url + '?callback=JSON_CALLBACK'
-			});
-		};	
-
-		// 3b. Haal details op voor 1 boek.
-		factory.getBookDetail = function (ean) {
-			return $http({
-				method: 'jsonp',
-				url: urlDetails + ean + '/00000000-0000-0000-0000-000000000000?callback=JSON_CALLBACK'
+				method: 'get',
+				url: url + 'avatar'
 			});
 		};
 
-		// 3c. Haal pagina op binnen 1 boek.
-		factory.getPage = function (bookID) {
+		// 3b. Haal details op voor 1 movie.
+		factory.getMovieDetail = function (id) {
 			return $http({
-				method: 'jsonp',
-				url: urlGetPage + bookID + '/0' + '?callback=JSON_CALLBACK'	// '0' is altijd de cover van een boek en die is by design altijd gratis toegankelijk. Andere/meer pagina's ophalen? Dan dit paginanummer dynamisch maken.
+				method: 'get',
+				url: urlDetails + id
 			});
 		};
-		
+
 		// 4. Altijd tot slot: retourneer het factory-object
 		return factory;
-	});
-})(angular.module('myApp'));
+	}
+})();
